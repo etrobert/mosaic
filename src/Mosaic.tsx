d86243f03b15baaf8f78aaef6a94311dee284661
erobert <etiennerobert33@gmail.com>
2022-01-23 18:00:45 +0100

    Display a grid in Mosaic

--- src/Mosaic.tsx
+++ src/Mosaic.tsx
@@ -1,11 +1,25 @@
+import type { CSSProperties } from "react";
 import type { Image } from "./types";
+
+import "./Mosaic.css";
 
 type Props = {
   image: Image;
 };
 
+const style = {
+  "--width": 50,
+  "--height": 50,
+} as CSSProperties;
+
 const Mosaic = ({ image }: Props) => {
-  return <>{image}</>;
+  return (
+    <div className="Mosaic" style={style}>
+      {image.flat().map((pixel, index) => (
+        <div key={index}>{pixel}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Mosaic;
