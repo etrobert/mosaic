50f0da91ba69a119f700f90867acd7a9055868fa
erobert <etiennerobert33@gmail.com>
2022-01-23 17:29:33 +0100

    Remove App content

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 import Contract from "web3-eth-contract";
@@ -21,24 +20,7 @@ const contract = new Contract(
 contract.methods.getImage().call().then(console.log);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
